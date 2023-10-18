993e31be8970681d5fbcd2b89474b27a0f0ed616
avigithub99 <145505312+avigithub99@users.noreply.github.com>
2023-10-18 17:19:55 -0500

    Add files via upload

---
+++ patient/user_home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include '../head_includes.php';?>
+    <?php include 'head_include.php';?>
+</head>
+<body >
+<?php 
+    include 'side_menu.php';
+ ?>
+
+
+<div class="main mt-5">
+  <div class="container text-center">
+    <h1>Health First Hospitals</h1>      
+    <h3>User Dashboard</h3>
+    
+  </div>
+</div>
+  
+
+
+<br><br>
+
+
+
+</body>
+</html>
